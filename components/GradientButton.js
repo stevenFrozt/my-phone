73fba63d696ef2f7fae8c411e547497c13205aff
Steven Kyle <90814089+stevenFrozt@users.noreply.github.com>
2022-07-18 21:33:03 +0800

    gradient radio button

--- components/GradientButton.js
+++ components/GradientButton.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react"
 
-export default function GradientButton({ text }) {
+export default function GradientButton({ text, toggle, setToggle }) {
   const [isSelect, setIsSelect] = useState(false)
   function selected() {
     setIsSelect((prev) => !isSelect)
     console.log("selected", isSelect)
+    setToggle(text)
   }
   const selectStyle = `bg-gradient-to-b from-black to-red-800 text-white text-center rounded-md p-2 text-sm truncate cursor-pointer lg:text-[1.2rem] lg:p-4`
   const defaultStyle =
     " bg-gradient-to-b from-black to-gray-300 text-white text-center rounded-md p-2 text-sm cursor-pointer truncate lg:text-[1.2rem] lg:p-4"
   return (
     <div
-      className={isSelect ? selectStyle : defaultStyle}
+      className={toggle == text ? selectStyle : defaultStyle}
       onClick={() => selected()}
     >
       {text}
--- pages/sell_details.js
+++ pages/sell_details.js
@@ -1,10 +1,17 @@
 import Link from "next/link"
-import React from "react"
+import React, { useState } from "react"
 import Footer from "../components/Footer"
 import GradientButton from "../components/GradientButton"
 import Navbar from "../components/Navbar"
 
-export default function sell_details() {
+export default function Sell_details() {
+  const [condition, setCondition] = useState()
+  const [carrier, setCarrier] = useState()
+  const [storage, setStorage] = useState()
+  const [phone, setPhone] = useState()
+  const [back, setBack] = useState()
+  const [front, setFront] = useState()
+  const [icloud, setIcloud] = useState()
   return (
     <div>
       <Navbar />
@@ -27,62 +34,126 @@ export default function sell_details() {
           <div className="pt-4">
             <h3 className="py-2">CONDITION</h3>
             <div className="grid-cols-3 grid gap-y-4 gap-x-2 lg:gap-x-4">
-              <GradientButton text="Brand New" />
-              <GradientButton text="Excellent" />
-              <GradientButton text="Good" />
-              <GradientButton text="Fair" />
-              <GradientButton text="Dead" />
+              <GradientButton
+                text="Brand New"
+                setToggle={setCondition}
+                toggle={condition}
+              />
+              <GradientButton
+                text="Excellent"
+                setToggle={setCondition}
+                toggle={condition}
+              />
+              <GradientButton
+                text="Good"
+                setToggle={setCondition}
+                toggle={condition}
+              />
+              <GradientButton
+                text="Fair"
+                setToggle={setCondition}
+                toggle={condition}
+              />
+              <GradientButton
+                text="Dead"
+                setToggle={setCondition}
+                toggle={condition}
+              />
             </div>
           </div>
           <div className="pt-4">
             <h3 className="py-2">CARRIER</h3>
             <div className="grid-cols-3 grid gap-y-4 gap-x-2 lg:gap-x-4">
-              <GradientButton text="UNLOCKED" />
-              <GradientButton text="LOCKED" />
+              <GradientButton
+                text="UNLOCKED"
+                setToggle={setCarrier}
+                toggle={carrier}
+              />
+              <GradientButton
+                text="LOCKED"
+                setToggle={setCarrier}
+                toggle={carrier}
+              />
             </div>
           </div>
           <div className="pt-4">
             <h3 className="py-2">STORAGE</h3>
             <div className="grid-cols-3 grid gap-y-4 gap-x-2 lg:gap-x-4">
-              <GradientButton text="128GB" />
-              <GradientButton text="256GB" />
-              <GradientButton text="512GB" />
-              <GradientButton text="1TB" />
+              <GradientButton
+                text="128GB"
+                setToggle={setStorage}
+                toggle={storage}
+              />
+              <GradientButton
+                text="256GB"
+                setToggle={setStorage}
+                toggle={storage}
+              />
+              <GradientButton
+                text="512GB"
+                setToggle={setStorage}
+                toggle={storage}
+              />
+              <GradientButton
+                text="1TB"
+                setToggle={setStorage}
+                toggle={storage}
+              />
             </div>
           </div>
           <div className="pt-4">
-            <h3 className="py-2 truncate">PHONE STATUS</h3>
+            <h3 className="py-2">PHONE STATUS</h3>
             <div className="grid-cols-3 grid gap-y-4 gap-x-2 lg:gap-x-4">
-              <GradientButton text="NO LOCKS" />
-              <GradientButton text="FINANCED" />
-              <GradientButton text="BLACKLISTED/BLOCKED" />
-              <GradientButton text="ACTIVATION LOCK" />
+              <GradientButton
+                text="NO LOCKS"
+                setToggle={setPhone}
+                toggle={phone}
+              />
+              <GradientButton
+                text="FINANCED"
+                setToggle={setPhone}
+                toggle={phone}
+              />
+              <GradientButton
+                text="BLACKLISTED/BLOCKED"
+                setToggle={setPhone}
+                toggle={phone}
+              />
+              <GradientButton
+                text="ACTIVATION LOCK"
+                setToggle={setPhone}
+                toggle={phone}
+              />
             </div>
           </div>
           <div className="pt-4">
             <h3 className="py-2">ARE THERE ANY CRACKS/CHIPS ON THE BACK?</h3>
             <div className="grid-cols-3 grid gap-y-4 gap-x-2 lg:gap-x-4">
-              <GradientButton text="YES" />
-              <GradientButton text="NO" />
+              <GradientButton text="YES" setToggle={setBack} toggle={back} />
+              <GradientButton text="NO" setToggle={setBack} toggle={back} />
             </div>
           </div>
           <div className="pt-4">
             <h3 className="py-2">ANY CRACKS/CHIPS ON FRONT SCREEN?</h3>
             <div className="grid-cols-3 grid gap-y-4 gap-x-2 lg:gap-x-4">
-              <GradientButton text="YES" />
-              <GradientButton text="NO" />
+              <GradientButton text="YES" setToggle={setFront} toggle={front} />
+              <GradientButton text="NO" setToggle={setFront} toggle={front} />
             </div>
           </div>
           <div className="pt-4">
             <h3 className="py-2">IS YOUR ICLOUD TURNED OFF?</h3>
             <div className="grid-cols-3 grid gap-y-4 gap-x-2 lg:gap-x-4">
-              <GradientButton text="YES" />
-              <GradientButton text="NO" />
+              <GradientButton
+                text="YES"
+                setToggle={setIcloud}
+                toggle={icloud}
+              />
+              <GradientButton text="NO" setToggle={setIcloud} toggle={icloud} />
             </div>
           </div>
           <div className="flex justify-center lg:justify-start py-8 mt-8">
             <Link href="sell_checkout">
-              <button className="bg-red-500 px-8 py-2 rounded text-center text-lg text-white lg:text-[1.2rem] lg:px-20 lg:py-4 lg:my-10" >
+              <button className="bg-red-500 px-8 py-2 rounded text-center text-lg text-white lg:text-[1.2rem] lg:px-20 lg:py-4 lg:my-10">
                 Checkout
               </button>
             </Link>
