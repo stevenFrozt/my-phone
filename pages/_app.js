d1e618494223efeb2b4dbb7d216fb8fab0b9ef12
Steven Kyle <90814089+stevenFrozt@users.noreply.github.com>
2022-07-19 14:22:49 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,9 @@
 import "../styles/globals.css"
 import { ThemeProvider } from "next-themes"
-import { useTheme } from "next-themes"
-import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }) {
-  const { theme, setTheme } = useTheme()
-
-  useEffect(() => {
-    setTheme("light")
-  }, [])
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" defaultTheme="light">
       <Component {...pageProps} />
     </ThemeProvider>
     // <Component {...pageProps} />
