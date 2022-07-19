8f3baf95398dd6c7264ff1667715c926debeba07
Steven Kyle <90814089+stevenFrozt@users.noreply.github.com>
2022-07-19 10:35:41 +0800

    tailwind autoprefixer added

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,20 @@
 import "../styles/globals.css"
 import { ThemeProvider } from "next-themes"
+import { useTheme } from "next-themes"
+import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }) {
-  
+  const { theme, setTheme } = useTheme()
+
+  useEffect(() => {
+    setTheme("light")
+  }, [])
   return (
     <ThemeProvider attribute="class">
-    <Component {...pageProps} />
+      <Component {...pageProps} />
     </ThemeProvider>
     // <Component {...pageProps} />
   )
-   
-
 }
 
 export default MyApp
