7173c58e5ff231a10acdbb3c43345d6c9360c6c4
Redemple32 <marceloredemple332@gmail.com>
2022-07-19 12:00:13 +0800

    repair done

--- pages/repair_date.js
+++ pages/repair_date.js
@@ -11,8 +11,88 @@ export default function repair_date() {
                     <div className="p-4 my-4 text-center font-medium lg:text-xl lg:my-8">
                         <h1>When do you want to come in?</h1>
                     </div>
+
+                    <h1>Choose a day.</h1>
+                    
+                    <div className="grid grid-cols-7 items-center my-8 gap-10 lg:gap-0">
+                        <div className="flex flex-col justify-center items-center">
+                            <h1 className="text-[#d42828] my-1">Tomorrow</h1>
+
+                            <div className="flex flex-col justify-center items-center rounded-xl border-4 w-16 h-16 lg:w-24 lg:h-24">
+                                <h1>Thu</h1>
+                                <h1>5/11</h1>
+                            </div>          
+                        </div>
+                        <div className="flex flex-col justify-center items-center">
+                            <h1 className="text-[#d42828] my-1 invisible">Tomorrow</h1>
+
+                            <div className="flex flex-col justify-center items-center rounded-xl border-4 w-16 h-16 lg:w-24 lg:h-24">
+                                <h1>Thu</h1>
+                                <h1>5/11</h1>
+                            </div>          
+                        </div>
+                        <div className="flex flex-col justify-center items-center">
+                            <h1 className="text-[#d42828] my-1 invisible">Tomorrow</h1>
+
+                            <div className="flex flex-col justify-center items-center rounded-xl border-4 w-16 h-16 lg:w-24 lg:h-24">
+                                <h1>Thu</h1>
+                                <h1>5/11</h1>
+                            </div>          
+                        </div>
+                        <div className="flex flex-col justify-center items-center">
+                            <h1 className="text-[#d42828] my-1 invisible">Tomorrow</h1>
+
+                            <div className="flex flex-col justify-center items-center rounded-xl border-4 w-16 h-16 lg:w-24 lg:h-24">
+                                <h1>Thu</h1>
+                                <h1>5/11</h1>
+                            </div>          
+                        </div>
+                        <div className="flex flex-col justify-center items-center">
+                            <h1 className="text-[#d42828] my-1 invisible">Tomorrow</h1>
+
+                            <div className="flex flex-col justify-center items-center rounded-xl border-4 w-16 h-16 lg:w-24 lg:h-24">
+                                <h1>Thu</h1>
+                                <h1>5/11</h1>
+                            </div>          
+                        </div>
+                        <div className="flex flex-col justify-center items-center">
+                            <h1 className="text-[#d42828] my-1 invisible">Tomorrow</h1>
+
+                            <div className="flex flex-col justify-center items-center rounded-xl border-4 w-16 h-16 lg:w-24 lg:h-24">
+                                <h1>Thu</h1>
+                                <h1>5/11</h1>
+                            </div>          
+                        </div>
+                        <div className="flex flex-col justify-center items-center">
+                            <h1 className="text-[#d42828] my-1 invisible">Tomorrow</h1>
+
+                            <div className="flex flex-col justify-center items-center rounded-xl border-4 w-16 h-16 lg:w-24 lg:h-24">
+                                <h1>Thu</h1>
+                                <h1>5/11</h1>
+                            </div>          
+                        </div>
+                    </div>
+
+                    <div className="grid grid-cols-2 my-8 gap-2">
+                        <div>
+                            <h1 className="invisible">Choose a date</h1>
+                            <div className="w-full border-2 rounded-md p-2">
+                                <input className="outline-0"
+                                type="date" />
+                            </div>
+                        </div>
+
+                        <div>
+                            <h1>Choose a time Thu 5/11.</h1>
+                            <div className="w-full border-2 rounded-md p-2">
+                            <input className="outline-0"
+                            type="time" />
+                        </div>
+                        </div>
+                        
+                    </div>
                     <Link href="repair_contact">
-                        <div className="p-5 bg-[#d42828] rounded-sm mb-4 ">
+                        <div className="bg-[#d42828] rounded-sm flex justify-center items-center h-16 mx-auto w-40">
                             <a className="text-center text-white">Continue</a>
                         </div>
                     </Link>
