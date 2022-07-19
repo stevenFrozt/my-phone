b25f32c53849ee66ffcaa8f5d2dd9a0ed04fa3c9
Steven Kyle <90814089+stevenFrozt@users.noreply.github.com>
2022-07-19 10:29:27 +0800

    client fixed

--- components/Device.js
+++ components/Device.js
@@ -6,8 +6,8 @@ export default function Device({ image, name, path }) {
     <Link href={path || ""}>
       <a>
         <div className=" p-4 flex flex-col justify-center items-center rounded-xl cursor-pointer hover:scale-110 transition-all duration-100">
-          <img src={image} alt="" />
-          <p className="text-sm py-2">{name}</p>
+          <img src={image} alt="" className="lg:h-36" />
+          <p className="text-sm py-2 text-center">{name}</p>
         </div>
       </a>
     </Link>
